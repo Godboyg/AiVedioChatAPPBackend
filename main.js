6120590d21349a821ee49f0b9f42d75b0799c94b
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-08 21:41:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const matchWithGemini = require("./ai.js");
 
 const io = new Server(server , {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
