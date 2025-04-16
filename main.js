93bfcbc197fc6dd4f0053aa8e29098b2b5c15af0
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-16 22:30:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const matchWithGemini = require("./ai.js");
 
 const io = new Server(server , {
   cors : {
-  origin: ['https://aivediochatapp.netlify.app'],
+  origin: ['https://deepmeet.netlify.app'],
   credentials: true,
   }
 });
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ['https://aivediochatapp.netlify.app'],
+  origin: ['https://deepmeet.netlify.app'],
   credentials: true,
 }));
 
